2717be37ed2a7e471538c3acff0870f566fc70e1
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-09-13 11:51:55 -0600

    Add tests for the gamesOfThronesReducer

--- src/reducers/gamesOfThrones-reducer.test.js
+++ src/reducers/gamesOfThrones-reducer.test.js
@@ -9,4 +9,14 @@ describe('gamesOfThronesReducer', () => {
 
     expect(result).toEqual(expected);
   })
+
+  it('should return an array of the games of thrones if action type is GET_GAMES_OF_THRONES', () => {
+    const expected = ['games', 'of', 'thrones'];
+
+    const action = {type: 'GET_GAMES_OF_THRONES', games: expected};
+
+    const result = gamesOfThronesReducer([], action);
+
+    expect(result).toEqual(expected);
+  })
 })
