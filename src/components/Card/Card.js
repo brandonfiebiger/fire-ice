cd67aefbd6ee2a6ed14042baf978f99907748792
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-09-13 13:31:04 -0600

    Successfully display swornMembers

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -10,21 +10,28 @@ export class Card extends Component {
   }
 
   getSwornMembers = async () => {
-    const { swornMembers } = this.props;
-    const memberPromises = swornMembers.map(async member => {
-      let memberId = member.slice(49)
-      const response = await fetch(`http://localhost:3001/api/v1/character/${memberId}`)
-      const fetchedMember = await response.json()
-      return fetchedMember
-    })
-    const fetchedSwornMembers = await Promise.all(memberPromises)
-    this.setState({
-      fetchedSwornMembers
-    })
-    console.log(this.state)
+    if(!this.state.fetchedSwornMembers.length) {
+      const { swornMembers } = this.props;
+      const memberPromises = swornMembers.map(async member => {
+        let memberId = member.slice(49)
+        const response = await fetch(`http://localhost:3001/api/v1/character/${memberId}`)
+        const fetchedMember = await response.json()
+        return fetchedMember
+      })
+      const fetchedSwornMembers = await Promise.all(memberPromises)
+      this.setState({
+        fetchedSwornMembers
+      })
+      console.log(this.state)
+    }
   }
 
+  
+  
   render() {
+    const displaySwornMembers = this.state.fetchedSwornMembers.map(member => {
+        return <p>{member.name}: {member.died ? member.died : 'Alive'}</p> || <p></p>
+      })
     const { name, founded, seats, titles, coatOfArms, ancestralWeapons, words} = this.props;
     return(
       <article className="Card" onClick={this.getSwornMembers}>
@@ -35,6 +42,7 @@ export class Card extends Component {
         <p>{ coatOfArms }</p>
         <p>{ ancestralWeapons }</p>
         <p>{ words }</p>
+        <section>{displaySwornMembers}</section>
       </article>
     )
   }
